f369d192147c02c22d59636240b8ae7bdf306265
Páll Hilmarsson <pallih@kaninka.net>
2012-08-07 00:56:29 +0000

    Added landspitali

---
+++ scrapers/landspitali.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import lxml.html
+import time
+import datetime
+import requests
+
+url = 'http://landspitali.is'
+html = requests.get(url).text
+root = lxml.html.fromstring(html)
+space = ' '
+strings = root.xpath('//div[@class="activityNumbers activityNumbersNew"]')
+for s in strings:
+    record = {}
+    record['date'] = time.strftime("%d.%m.%Y", time.gmtime())
+    record['time'] = time.strftime("%H:%M", time.gmtime())
+    for d in s[1:]:
+        record[d.attrib['class']] =  d[1].text
+print record
