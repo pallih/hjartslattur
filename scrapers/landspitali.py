65f6e1abe1d57079e865e593ee24f84b54da2526
Páll Hilmarsson <pallih@kaninka.net>
2012-08-07 22:17:09 +0000

    landspitali update

--- scrapers/landspitali.py
+++ scrapers/landspitali.py
@@ -6,14 +6,16 @@ import datetime
 import requests
 
 url = 'http://landspitali.is'
-html = requests.get(url).text
-root = lxml.html.fromstring(html)
-space = ' '
-strings = root.xpath('//div[@class="activityNumbers activityNumbersNew"]')
-for s in strings:
-    record = {}
-    record['date'] = time.strftime("%d.%m.%Y", time.gmtime())
-    record['time'] = time.strftime("%H:%M", time.gmtime())
-    for d in s[1:]:
-        record[d.attrib['class']] =  d[1].text
-print record
+
+def landspitali():
+	html = requests.get(url).text
+	root = lxml.html.fromstring(html)
+	space = ' '
+	strings = root.xpath('//div[@class="activityNumbers activityNumbersNew"]')
+	for s in strings:
+		record = {}
+		record['date'] = time.strftime("%d.%m.%Y", time.gmtime())
+		record['time'] = time.strftime("%H:%M", time.gmtime())
+		for d in s[1:]:
+			record[d.attrib['class']] =  d[1].text
+	return record
